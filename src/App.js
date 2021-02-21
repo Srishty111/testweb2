4266345c7948aa59ed13a76b360b18b3923e6a40
Srishty112 <srishty.bhardwaj.17@gmail.com>
2021-02-21 15:01:39 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect,useState} from 'react';
-import Login from './Component/Login'
-import Home from './Component/Home'
-import Signup from './Component/Signup'
-import Home1 from './Component/Home1'
-import CreatePlan from './Component/CreatePlan'
-import {useDispatch, useSelector} from 'react-redux'
-import {createStore,combineReducers} from 'redux'
-import reducer from './reducer/Reducer'
-import {Provider} from 'react-redux'
-import Check from './Component/Check'
-import CreatePlan1 from './Component/Createplan1'
-import Plan from './Component/Plan'
-import ViewMore from './Component/ViewMore';
-import EditPlan from './Component/EditPlan'
+import React, { Component } from 'react'
+import './App.css'
 
-import {
-  BrowserRouter ,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useHistory
-} from "react-router-dom";
-import MyPlan from './Component/SMyPlan';
+function App(){
 
-const store=createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-function App() {
+  var array=[0,0];
+  var a2=0
 
- 
-  return (
-      <>
-    <Provider store={store}>
-    <BrowserRouter>
-          <Switch>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/signup" component={Signup}></Route>
-            <Route exact path="/home" component={Home}></Route> 
-            <Route exact path="/home/selectExercise" component={Home1}></Route>
-            <Route path="/createplan" component={CreatePlan1}></Route>
-            <Route path="/plan" component={Plan}></Route>
-            <Route path="/viewmore" component={ViewMore}></Route>
-            <Route path="/editplan" component={EditPlan}></Route>
-            <Route path="/" component={MyPlan}></Route>
-          </Switch>
-    
-    </BrowserRouter>
-    <Check></Check>
-    
-    </Provider>     
-    </>
-     );
-}
 
-export default App;
+
+  function handleClick(e) {
+    var a= e.target.id
+    if(array[0]!=a && array[1]!=a){
+    a2=array[0]
+    array[0]=array[1]; array[1]=a;
+
+    if(document.getElementById(array[0]))document.getElementById(array[0]).style.backgroundColor='blue'
+    if(document.getElementById(array[1]))document.getElementById(array[1]).style.backgroundColor='blue'
+    if(document.getElementById(a2))document.getElementById(a2).style.backgroundColor='red'
+  }}
+    return (
+      <div class="col">
+      <br/>
+      <br/>
+      <br/>
+        <div class="row">
+        <div class="col" style={{'backgroundColor': 'red'}} id={1} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={2} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={3} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={4} onClick={handleClick}>.</div>
+        </div>
+        <div class="row">
+        <div class="col" style={{'backgroundColor': 'red'}} id={5} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={6} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={7} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={8} onClick={handleClick}>.</div>
+        </div>
+        <div class="row">
+        <div class="col" style={{'backgroundColor': 'red'}} id={9} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={10} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={11} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={12} onClick={handleClick}>.</div>
+        </div>
+        <div class="row">
+        <div class="col" style={{'backgroundColor': 'red'}} id={13} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={14} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={15} onClick={handleClick}></div>
+        <div class="col" style={{'backgroundColor': 'red'}} id={16} onClick={handleClick}>.</div>
+        </div>
+  
+      </div>
+    )
+  }
+
+export default App
